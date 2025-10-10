a8d9ed8f248d0655d828340df9b42c9f3636894f
Hsoren31 <sorensenhaley@gmail.com>
2025-10-10 14:24:55 -0600

    FEAT: connect post blog to backend api

--- blog/src/pages/Post.jsx
+++ blog/src/pages/Post.jsx
@@ -1,40 +1,53 @@
-const post = {
-  id: 3,
-  title: "How to Write Titles",
-  description: "Just write it",
-  body: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
-  timestamp: "2/13/2025",
-  comments: [
-    {
-      id: 34,
-      author: "user567",
-      timestamp: "5/2/2025",
-      message: "Great Post",
-    },
-  ],
-};
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 export default function Post() {
+  const { postId } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`http://localhost:3000/posts/${postId}`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      if (!response.ok) {
+        throw new Error("HTTP Error: Status " + response.status);
+      }
+      let postData = await response.json();
+      setPost(postData.post);
+    }
+    fetchData();
+  }, [postId]);
+
   return (
     <>
-      <h1>{post.title}</h1>
-      <p>{post.timestamp}</p>
-      <p>{post.description}</p>
-      <p>{post.body}</p>
-      {post.comments ? (
-        <div>
-          <h3>Comments</h3>
-          <ul>
-            {post.comments.map((comment) => (
-              <div key={comment.id}>
-                <p>{comment.author}</p>
-                <p>{comment.timestamp}</p>
-                <p>{comment.message}</p>
-              </div>
-            ))}
-          </ul>
-        </div>
+      {post ? (
+        <>
+          <h1>{post.title}</h1>
+          <p>{post.timestamp}</p>
+          <p>{post.description}</p>
+          <p>{post.body}</p>
+          {post.comments ? (
+            <div>
+              <h3>Comments</h3>
+              <ul>
+                {post.comments.map((comment) => (
+                  <div key={comment.id}>
+                    <p>{comment.author}</p>
+                    <p>{comment.timestamp}</p>
+                    <p>{comment.message}</p>
+                  </div>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p>No Comments Yet.</p>
+          )}
+        </>
       ) : (
-        <p>No Comments Yet.</p>
+        <p>Unable to retrieve post.</p>
       )}
     </>
   );
