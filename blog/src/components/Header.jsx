ffba3058e27d3db4e1af2744b44a23fb5699b4cf
Hsoren31 <sorensenhaley@gmail.com>
2025-10-17 09:26:35 -0600

    FEAT: (blog) add logout link with functionality

--- blog/src/components/Header.jsx
+++ blog/src/components/Header.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  function logoutUser() {
+    localStorage.clear();
+  }
+
   return (
     <header>
       <h1>Blog</h1>
@@ -8,6 +12,11 @@ export default function Header() {
         <ul>
           <li>
             <Link to="/">Home</Link>
+          </li>
+          <li>
+            <a href="/login" onClick={logoutUser}>
+              Logout
+            </a>
           </li>
         </ul>
       </nav>
