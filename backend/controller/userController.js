f625da34616e037f0ecb488ef3617f11c06f67fc
Hsoren31 <sorensenhaley@gmail.com>
2025-11-06 09:57:49 -0700

    FEAT: add validation for new user input
    To prevent receiving the wrong data when new users create an account.
    This adds validators to each of the fields, (first and last name, email,
    username, password, and the password confirmation) to check if they meet
    the requirements.

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,33 +1,90 @@
 const db = require("../db/userQueries");
 const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
 
-async function createUser(req, res) {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const { firstName, lastName, email, username } = req.body;
-    // check if user exists in the database
-    const userExists = await db.findIfUserExists(email);
-    if (userExists) {
-      return res.status(400).json({ error: "User already exists" });
+const validateUser = [
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .withMessage("First name must not be empty")
+    .isAlpha()
+    .withMessage("First name must only contain alphabet letters.")
+    .isLength({ min: 1, max: 20 })
+    .withMessage("First name must be between 1 and 20 characters"),
+  body("lastName")
+    .trim()
+    .notEmpty()
+    .withMessage("Last name must not be empty")
+    .isAlpha()
+    .withMessage("Last name must only contain alphabet letters.")
+    .isLength({ min: 1, max: 20 })
+    .withMessage("Last name must be between 1 and 20 characters"),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email can not be empty.")
+    .isEmail()
+    .withMessage("Must be a valid Email"),
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username must not be empty.")
+    .isLength({ min: 8, max: 15 })
+    .withMessage("Username must be between 8 and 15 characters"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password can not be empty")
+    .isLength({ min: 8, max: 15 })
+    .withMessage("Password must be between 8 and 15 characters")
+    .isStrongPassword({
+      minLength: 8,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 0,
+    })
+    .withMessage("Password must contain one Uppercase Letter and one Number."),
+  body("confirmPassword")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords must match."),
+];
+
+createUser = [
+  validateUser,
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json(errors);
+      }
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const { firstName, lastName, email, username } = req.body;
+      // check if user exists in the database
+      const userExists = await db.findIfUserExists(email);
+      if (userExists) {
+        return res.status(400).json({ error: "User already exists" });
+      }
+      // check if username is taken
+      const usernameExists = await db.findIfUsernameExists(username);
+      if (usernameExists) {
+        return res.status(400).json({ error: "Username is already taken." });
+      }
+      const user = await db.createUser(
+        firstName,
+        lastName,
+        email,
+        username,
+        hashedPassword
+      );
+      res.json({ user });
+    } catch (error) {
+      console.error(error);
+      res.json({ error: "Something went wrong. Try again." });
     }
-    // check if username is taken
-    const usernameExists = await db.findIfUsernameExists(username);
-    if (usernameExists) {
-      return res.status(400).json({ error: "Username is already taken." });
-    }
-    const user = await db.createUser(
-      firstName,
-      lastName,
-      email,
-      username,
-      hashedPassword
-    );
-    res.json({ user });
-  } catch (error) {
-    console.error(error);
-    res.json({ error: "Something went wrong. Try again." });
-  }
-}
+  },
+];
 
 async function readUser(req, res) {
   try {
