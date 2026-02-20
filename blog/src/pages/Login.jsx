0a7d2835b0f823a86e7221e3f2e1f70fb3c2087f
Hsoren31 <sorensenhaley@gmail.com>
2026-02-20 07:16:22 -0700

    FEAT: add opposing links to blog login/signup page
    If the user is on the login in page, there is a link to navigate to the
    signup page if they needed to signup a new account. Vice versa with the
    signup page, it has a link to the login page.

--- blog/src/pages/Login.jsx
+++ blog/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext } from "react";
 import { CurrentUserContext } from "../context/CurrentUserContext";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   const { login } = useContext(CurrentUserContext);
@@ -16,40 +17,43 @@ export default function Login() {
   };
 
   return (
-    <form onSubmit={loginUser}>
-      {error && <p>{error}</p>}
-      <legend>Login</legend>
-      <div>
-        <label htmlFor="username">Username: </label>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={user.username}
-          onChange={(e) => {
-            setUser((prevData) => ({
-              ...prevData,
-              username: e.target.value,
-            }));
-          }}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={user.password}
-          onChange={(e) => {
-            setUser((prevData) => ({
-              ...prevData,
-              password: e.target.value,
-            }));
-          }}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={loginUser}>
+        {error && <p>{error}</p>}
+        <legend>Login</legend>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={user.username}
+            onChange={(e) => {
+              setUser((prevData) => ({
+                ...prevData,
+                username: e.target.value,
+              }));
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={user.password}
+            onChange={(e) => {
+              setUser((prevData) => ({
+                ...prevData,
+                password: e.target.value,
+              }));
+            }}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <Link to="/signup">Don't have an account? Sign up here.</Link>
+    </>
   );
 }
--- blog/src/pages/Signup.jsx
+++ blog/src/pages/Signup.jsx
@@ -1,32 +1,37 @@
+import { Link } from "react-router-dom";
+
 export default function Signup() {
   return (
-    <form>
-      <legend>Sign Up</legend>
-      <div>
-        <label htmlFor="firstName">First Name: </label>
-        <input type="firstName" name="firstName" id="firstName" />
-      </div>
-      <div>
-        <label htmlFor="lastName">Last Name: </label>
-        <input type="text" name="lastName" id="lastName" />
-      </div>
-      <div>
-        <label htmlFor="email">Email: </label>
-        <input type="email" name="email" id="email" />
-      </div>
-      <div>
-        <label htmlFor="username">Username: </label>
-        <input type="text" name="username" id="username" />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input type="password" name="password" id="password" />
-      </div>
-      <div>
-        <label htmlFor="confirmPassword">Confirm Password: </label>
-        <input type="password" name="confirmPassword" id="confirmPassword" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form>
+        <legend>Sign Up</legend>
+        <div>
+          <label htmlFor="firstName">First Name: </label>
+          <input type="firstName" name="firstName" id="firstName" />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name: </label>
+          <input type="text" name="lastName" id="lastName" />
+        </div>
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input type="text" name="username" id="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password: </label>
+          <input type="password" name="confirmPassword" id="confirmPassword" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <Link to="/login">Have an account already? Login here.</Link>
+    </>
   );
 }
