1f6e0fe2d96f34673c629df8fb2f5000cc0bdd02
Hsoren31 <sorensenhaley@gmail.com>
2026-06-30 10:20:02 -0600

    FEAT: send user info for frontend context on login

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -99,9 +99,10 @@ async function postLogin(req, res) {
     if (!user) {
       res.status(400).json({ message: options.message });
     }
-    user = { id: user.id, username: user.username };
+    const body = { id: user.id, username: user.username };
     jwt.sign(user, process.env.SECRET_KEY, (err, token) => {
       res.json({
+        body,
         token,
       });
     });
