3ce881749bc4440c83e96f9d9824a47248a30a5e
Hsoren31 <sorensenhaley@gmail.com>
2026-07-06 12:22:14 -0600

    CHORE: remove deleted files

--- author/src/PrivateRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoutes = () => {
-  let auth = localStorage.getItem("token");
-
-  return auth ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoutes;
--- author/src/context/CurrentUserContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-export const CurrentUserContext = createContext(null);
-export const CurrentUserDispatchContext = createContext(null);
