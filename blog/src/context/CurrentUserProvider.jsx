3f6e8b145eda42aa11580844a1a0a90928d01a18
Hsoren31 <sorensenhaley@gmail.com>
2026-07-15 14:06:28 -0600

    REFACTOR: remove signup/login requests
    With the new signup/login hooks these aren't needed, and the context is
    updated within the login hook.

--- blog/src/context/CurrentUserProvider.jsx
+++ blog/src/context/CurrentUserProvider.jsx
@@ -1,53 +1,19 @@
 import { useState } from "react";
 import { CurrentUserContext } from "./CurrentUserContext";
-import { Navigate, useNavigate } from "react-router-dom";
-import { loginUser } from "../utilities/apiRequests";
+import { useNavigate } from "react-router-dom";
 
 function useCurrentUser() {
   const navigate = useNavigate();
   const user = localStorage.getItem("user");
   const [currentUser, setCurrentUser] = useState(user ? user : null);
 
-  async function register(credentials) {
-    try {
-      const response = await fetch(`http://localhost:3000/users`, {
-        method: "POST",
-        body: JSON.stringify(credentials),
-      });
-      const data = await response.json();
-      if (!response.ok) {
-        if (response.status === 400) {
-          throw new Error(data.message);
-        }
-        throw new Error(`HTTP error: Status ${response.status}`);
-      }
-      navigate("/login");
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  }
-
-  async function login(userData) {
-    try {
-      const data = await loginUser(userData);
-      localStorage.setItem("user", data.user);
-      localStorage.setItem("token", data.token);
-      setCurrentUser(data.user);
-      navigate("/");
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  }
-
   async function logout() {
     localStorage.clear();
     setCurrentUser(null);
-    Navigate("/");
+    navigate("/");
   }
 
-  return { currentUser, setCurrentUser, register, login, logout };
+  return { currentUser, setCurrentUser, logout };
 }
 
 export function CurrentUserProvider({ children }) {
