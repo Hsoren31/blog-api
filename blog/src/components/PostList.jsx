779079c265ba962b595bb599eae898c45a20e46b
Hsoren31 <sorensenhaley@gmail.com>
2026-07-16 13:08:34 -0600

    FIX: remove link wrapping post list item

--- blog/src/components/PostList.jsx
+++ blog/src/components/PostList.jsx
@@ -8,26 +8,24 @@ export default function PostList({ posts }) {
         <ul>
           {posts.map((post) => (
             <li key={post.id} className="post-list-item">
-              <Link to={`/posts/${post.id}`}>
-                <div className="post_frame">
-                  {post.tags && (
-                    <ul>
-                      {post.tags.map((tag) => (
-                        <li key={tag.id}>{tag.name}</li>
-                      ))}
-                    </ul>
-                  )}
-                </div>
-                <p>
-                  {post.author && <>{post.author.username} &middot; </>}
-                  {formatDateDistance(
-                    post.updatedAt ? post.updatedAt : post.createdAt
-                  )}
-                </p>
-                <h2>{post.title}</h2>
-                <p>{post.description}</p>
-                <Link to={`/posts/${post.id}`}>Read more...</Link>
-              </Link>
+              <div className="post_frame">
+                {post.tags && (
+                  <ul>
+                    {post.tags.map((tag) => (
+                      <li key={tag.id}>{tag.name}</li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+              <p>
+                {post.author && <>{post.author.username} &middot; </>}
+                {formatDateDistance(
+                  post.updatedAt ? post.updatedAt : post.createdAt
+                )}
+              </p>
+              <h2>{post.title}</h2>
+              <p>{post.description}</p>
+              <Link to={`/posts/${post.id}`}>Read more...</Link>
             </li>
           ))}
         </ul>
