5dad4ab7dfbc613afcae953fc9f4a8f9da70f177
Hsoren31 <sorensenhaley@gmail.com>
2026-07-27 13:00:20 -0600

    FEAT: layout home page post list

--- blog/src/components/PostList.jsx
+++ blog/src/components/PostList.jsx
@@ -5,27 +5,31 @@ export default function PostList({ posts }) {
   return (
     <>
       {posts ? (
-        <ul>
+        <ul id="posts">
           {posts.map((post) => (
             <li key={post.id} className="post-list-item">
               <div className="post_frame">
                 {post.tags && (
-                  <ul>
+                  <ul className="tags">
                     {post.tags.map((tag) => (
-                      <li key={tag.id}>{tag.name}</li>
+                      <li className="tag" key={tag.id}>
+                        {tag.name}
+                      </li>
                     ))}
                   </ul>
                 )}
               </div>
-              <p>
-                {post.author && <>{post.author.username} &middot; </>}
-                {formatDateDistance(
-                  post.updatedAt ? post.updatedAt : post.createdAt
-                )}
-              </p>
-              <h2>{post.title}</h2>
-              <p>{post.description}</p>
-              <Link to={`/posts/${post.id}`}>Read more...</Link>
+              <div className="post_content">
+                <p>
+                  {post.author && <>{post.author.username} &middot; </>}
+                  {formatDateDistance(
+                    post.updatedAt ? post.updatedAt : post.createdAt
+                  )}
+                </p>
+                <h2>{post.title}</h2>
+                <p>{post.description}</p>
+                <Link to={`/posts/${post.id}`}>Read more...</Link>
+              </div>
             </li>
           ))}
         </ul>
