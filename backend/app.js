0e7d481e3bd10f92fcb572fccbe9301ee58b0f11
Hsoren31 <sorensenhaley@gmail.com>
2026-08-12 13:38:54 -0600

    FEAT: add array of origin for cors

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,17 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({
+    origin: [
+      process.env.BLOG_URL,
+      process.env.AUTHOR_URL,
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ],
+    credentials: true,
+  })
+);
 
 app.use("/api/posts", postsRouter);
 app.use("/api/users", usersRouter);
